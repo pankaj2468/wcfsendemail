c73f2ef91207020d0b81c1562a90ea77c6692043
pankajy2 <pankajy2@CHETUIWK1857>
2022-07-19 22:20:14 +0530

    Add project files.

---
+++ wcfsendemail/IService1.cs
@@ -0,0 +1,85 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wcfsendemail
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string SendEmail(string gmailUserAddress, string gmailUserPassword,
+        string emailTo, string ccTo, string subject, string body, bool isBodyHtml);
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "wcfsendemail.ContractType".
+    //[DataContract]
+    //public class SendEmail
+    //{
+    //    string gmailUserName = string.Empty;
+    //    string gmailUserPassword = string.Empty;
+    //    string emailTo = string.Empty;
+    //    string ccTo = string.Empty;
+    //    string subject = string.Empty;
+    //    string body = string.Empty;
+    //    //bool isBodyHtml = string.IsBodyHtml;
+    //    //string empid;
+
+    //    [DataMember]
+    //    public string UserName
+    //    {
+    //        get { return gmailUserName; }
+    //        set { gmailUserName = value; }
+    //    }
+    //    [DataMember]
+    //    public string Password
+    //    {
+    //        get { return gmailUserPassword; }
+    //        set { gmailUserPassword = value; }
+    //    }
+    //    [DataMember]
+    //    public string EmailTo
+    //    {
+    //        get { return emailTo; }
+    //        set { emailTo = value; }
+    //    }
+
+    //    [DataMember]
+    //    public string CCTo
+    //    {
+    //        get { return ccTo; }
+    //        set { ccTo = value; }
+    //    }
+
+    //    [DataMember]
+    //    public string Subject
+    //    {
+    //        get { return subject; }
+    //        set { subject = value; }
+    //    }
+
+    //    [DataMember]
+    //    public string IsBodyHtml
+    //    {
+    //        get { return IsBodyHtml; }
+    //        set { IsBodyHtml = value; }
+    //    }
+
+
+    //    [DataMember]
+    //    public string Body
+    //    {
+    //        get { return body; }
+    //        set { body = value; }
+    //    }
+
+
+    //}
+}
---
+++ wcfsendemail/Service1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wcfsendemail
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+       
+            private static string SMTPSERVER = "smtp.office365.com";
+            private static int PORTNO = 587;
+
+            public string SendEmail(string gmailUserName, string gmailUserPassword,
+               string emailToAddress, string ccemailTo, string subject, string body, bool isBodyHtml)
+            {
+                if (gmailUserName == null || gmailUserName.Trim().Length == 0)
+                {
+                    return "User Name Empty";
+                }
+                if (gmailUserPassword == null || gmailUserPassword.Trim().Length == 0)
+                {
+                    return "Email Password Empty";
+                }
+                if (emailToAddress == null || emailToAddress.Length == 0)
+                {
+                    return "Email To Address Empty";
+                }
+
+                List<string> tempFiles = new List<string>();
+
+                SmtpClient smtpClient = new SmtpClient(SMTPSERVER, PORTNO);
+                smtpClient.EnableSsl = true;
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential("leaves@chetu.com", "cHLeaV@2021");
+                using (MailMessage message = new MailMessage())
+                {
+                    message.From = new MailAddress("leaves@chetu.com");
+                    message.Subject = subject == null ? "" : subject;
+                    message.Body = body == null ? "" : body;
+                    message.IsBodyHtml = isBodyHtml;
+
+                //foreach (string email in emailToAddress)
+                //    {
+                        message.To.Add(emailToAddress);
+                    //}
+                    if (ccemailTo != null && ccemailTo.Length > 0)
+                    {
+                        //foreach (string emailCc in ccemailTo)
+                        //{
+                            message.CC.Add(ccemailTo);
+                        //}
+                    }
+                    try
+                    {
+                        smtpClient.Send(message);
+                        return "Email Send SuccessFully";
+                    }
+                    catch(Exception Ex)
+                    {
+                        return "Email Send failed"+Ex.ToString();
+                    }
+                    finally
+                    {
+                    smtpClient.Send(message);
+                    //return "Email Send SuccessFully";
+                    }   
+                }
+            }
+        
+    }
+}
